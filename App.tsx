e4a1ddd9aa36baf1ca5e5abe76529527c5fcf193
Ridvan Dzh <63258916+rddeveloper2019@users.noreply.github.com>
2025-08-15 21:02:55 +0300

    4.2

--- App.tsx
+++ App.tsx
@@ -6,7 +6,9 @@ export default function App() {
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!!!</Text>
       <Text>Open up App.tsx to start working on your app!!!</Text>
-      <Text>Open up App.tsx to start working on your app!!!</Text>
+      <Text style={styles.textStyle}>
+        Open up App.tsx to start working on your app!!!
+      </Text>
       <StatusBar style="auto" />
       <TextInput placeholder="useless placeholder" keyboardType="numeric" />
       <Button
@@ -21,8 +23,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "#f86af6ff",
     alignItems: "center",
     justifyContent: "center",
+  },
+  textStyle: {
+    color: "green",
+    fontSize: 24,
+    fontWeight: 500,
   },
 });
