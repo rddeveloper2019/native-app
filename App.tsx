00cdbb03d6d63776db7f75b09ae5340cac8143e7
Ridvan Dzh <63258916+rddeveloper2019@users.noreply.github.com>
2025-08-15 21:26:21 +0300

    4.4

--- App.tsx
+++ App.tsx
@@ -4,32 +4,48 @@ import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!!!</Text>
-      <Text>Open up App.tsx to start working on your app!!!</Text>
-      <Text style={styles.textStyle}>
-        Open up App.tsx to start working on your app!!!
-      </Text>
-      <StatusBar style="auto" />
-      <TextInput placeholder="useless placeholder" keyboardType="numeric" />
-      <Button
-        title="Learn More"
-        color="#841584"
-        accessibilityLabel="Learn more about this purple button"
-      />
+      <View style={styles.flexContainer}>
+        <View style={{ ...styles.box, ...styles.red }}>
+          <Text style={styles.text}>1</Text>
+        </View>
+        <View style={{ ...styles.box, ...styles.orange }}>
+          <Text style={styles.text}>2</Text>
+        </View>
+        <View style={{ ...styles.box, ...styles.green }}>
+          <Text style={styles.text}>3</Text>
+        </View>
+        <View style={{ ...styles.box, ...styles.pink }}>
+          <Text style={styles.text}>4</Text>
+        </View>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: "#f86af6ff",
-    alignItems: "center",
-    justifyContent: "center",
+    marginTop: 90,
   },
-  textStyle: {
-    color: "green",
-    fontSize: 24,
+  text: {
+    color: "#fff",
+    fontSize: 20,
     fontWeight: 500,
   },
+  flexContainer: {
+    display: "flex",
+    flexDirection: "column",
+    backgroundColor: "yellow",
+    alignItems: "center",
+    justifyContent: "space-around",
+    height: 600,
+  },
+  box: {
+    // width: 100,
+    height: 100,
+    padding: 40,
+  },
+  green: { backgroundColor: "green" },
+  red: { backgroundColor: "red" },
+  orange: { backgroundColor: "orange" },
+  pink: { backgroundColor: "pink" },
 });
