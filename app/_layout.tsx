9be728fab739d6c1bfd5f528b48c478634e7b767
Ridvan Dzh <63258916+rddeveloper2019@users.noreply.github.com>
2025-08-19 21:34:05 +0300

    8.4 Stack

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,9 @@
-import { Slot, Tabs } from 'expo-router';
+import { Slot, Stack, Tabs } from 'expo-router';
 import React from 'react';
 import { View, Text } from 'react-native';
 
 const RootLayout = () => {
-  return <Tabs />;
+  return <Stack />;
 };
 
 export default RootLayout;
