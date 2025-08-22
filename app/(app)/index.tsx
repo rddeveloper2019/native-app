9afe50489e27c6c10d11eb58916a67bb80a78af3
Ridvan Dzh <63258916+rddeveloper2019@users.noreply.github.com>
2025-08-22 19:04:10 +0300

    9.9 redirect

--- app/(app)/index.tsx
+++ app/(app)/index.tsx
@@ -1,34 +1,35 @@
 import { View } from 'react-native';
 import { AppLink } from '../../shared/AppLink';
 import { AppText } from '../../shared/AppText';
-import { useAtom, useSetAtom } from 'jotai';
+import { useAtom, useAtomValue, useSetAtom } from 'jotai';
 import { profileAtom } from '../../entities/user/model/user.state';
 import { useEffect } from 'react';
-import { loginAtom, logoutAtom } from '../../entities/auth/model/auth.model';
+import {
+  authAtom,
+  loginAtom,
+  logoutAtom,
+} from '../../entities/auth/model/auth.model';
+import { router, useRootNavigationState } from 'expo-router';
 
 export const MyCourses = () => {
-  const [{ profile }] = useAtom(profileAtom);
+  // const [{ profile }] = useAtom(profileAtom);
 
-  const [{ accessToken, isLoading, error }, login] = useAtom(loginAtom);
-  const logout = useSetAtom(logoutAtom);
+  // const [{ accessToken, isLoading, error }, login] = useAtom(loginAtom);
+  // const logout = useSetAtom(logoutAtom);
+  const state = useRootNavigationState();
+
+  const { accessToken } = useAtomValue(authAtom);
 
   useEffect(() => {
-    login({
-      email: 'vasia@pupkin.ru',
-      password: '12345678',
-    });
-
-    setTimeout(() => {
-      logout();
-    }, 6000);
-  }, []);
+    if (!accessToken && state.key) {
+      router.replace('login');
+    }
+  }, [accessToken, state]);
 
   return (
     <View>
       <AppText>My Courses</AppText>
-      <AppText>{profile?.name}</AppText>
-      <AppText>{accessToken}</AppText>
-      <AppText>{error}</AppText>
+
       <AppLink href={'/course/typescript'} text="Перейти к курсу" />
       <AppLink href={'/login'} text="Перейти к логину" />
     </View>
