ef52cbc6cb78b4f54e9541db677b3d0d2386f1f1
Ridvan Dzh <63258916+rddeveloper2019@users.noreply.github.com>
2025-08-22 19:08:13 +0300

    9.10 component redirect

--- app/(app)/_layout.tsx
+++ app/(app)/_layout.tsx
@@ -1,10 +1,18 @@
-import { Stack } from 'expo-router';
+import { Redirect, Stack } from 'expo-router';
 import { Button } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { Colors } from '../../shared/tokens';
 import { StatusBar } from 'expo-status-bar';
+import { useAtomValue } from 'jotai';
+import { authAtom } from '../../entities/auth/model/auth.model';
 
 const AppLayout = () => {
+  const { accessToken } = useAtomValue(authAtom);
+
+  if (!accessToken) {
+    return <Redirect href={'/login'} />;
+  }
+
   return (
     <Stack
       screenOptions={{
--- app/(app)/index.tsx
+++ app/(app)/index.tsx
@@ -1,31 +1,8 @@
 import { View } from 'react-native';
 import { AppLink } from '../../shared/AppLink';
 import { AppText } from '../../shared/AppText';
-import { useAtom, useAtomValue, useSetAtom } from 'jotai';
-import { profileAtom } from '../../entities/user/model/user.state';
-import { useEffect } from 'react';
-import {
-  authAtom,
-  loginAtom,
-  logoutAtom,
-} from '../../entities/auth/model/auth.model';
-import { router, useRootNavigationState } from 'expo-router';
 
 export const MyCourses = () => {
-  // const [{ profile }] = useAtom(profileAtom);
-
-  // const [{ accessToken, isLoading, error }, login] = useAtom(loginAtom);
-  // const logout = useSetAtom(logoutAtom);
-  const state = useRootNavigationState();
-
-  const { accessToken } = useAtomValue(authAtom);
-
-  useEffect(() => {
-    if (!accessToken && state.key) {
-      router.replace('login');
-    }
-  }, [accessToken, state]);
-
   return (
     <View>
       <AppText>My Courses</AppText>
