2be446e69b56830da0a1128af12fd7f19e4b0ab7
Ridvan Dzh <63258916+rddeveloper2019@users.noreply.github.com>
2025-08-22 17:50:53 +0300

    9.4 async storage

--- app/(app)/index.tsx
+++ app/(app)/index.tsx
@@ -3,9 +3,22 @@ import { AppLink } from '../../shared/AppLink';
 import { AppText } from '../../shared/AppText';
 import { useAtom } from 'jotai';
 import { profileAtom } from '../../entities/user/model/user.state';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { useEffect } from 'react';
 
 export const MyCourses = () => {
   const [{ profile }] = useAtom(profileAtom);
+
+  useEffect(() => {
+    AsyncStorage.setItem('demo', 'xxxDEMOxxx').then(async () => {
+      console.log(await AsyncStorage.getAllKeys());
+      console.log(await AsyncStorage.getItem('demo'));
+      console.log(await AsyncStorage.removeItem('demo'));
+      console.log(await AsyncStorage.getAllKeys());
+      console.log(await AsyncStorage.getItem('demo'));
+    });
+  }, []);
+
   return (
     <View>
       <AppText>My Courses</AppText>
